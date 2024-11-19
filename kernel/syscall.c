70880c69858b3398bc38f38392bb33d6aab0826e
larryyu2004 <jiayiyu@myyahoo.com>
2024-11-19 17:43:24 +1030

    Lab2

--- kernel/syscall.c
+++ kernel/syscall.c
@@ -149,12 +149,7 @@ syscall(void)
     if((p->trace_mask>>num) & 1){
       //3: syscall read -> 1023
       printf("%d: syscall %s -> %d\n", p->pid, syscall_name[num], p->trapframe->a0);
-  }else{
-    printf("%d %s: unknow syscall %d\n", p->pid, p->name, num);
-    p->trapframe->a0 = -1;
-  }
-    
-    
+    }
   } else {
     printf("%d %s: unknown sys call %d\n",
             p->pid, p->name, num);
